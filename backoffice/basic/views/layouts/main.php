9229777db03db7bca12b6cb5313e99d82cba71cc
Alex <alex@alex-design.fr>
2020-04-29 22:53:09 +0200

    Mise en page est habillage CSS

--- backoffice/basic/views/layouts/main.php
+++ backoffice/basic/views/layouts/main.php
@@ -102,7 +102,7 @@ AppAsset::register($this);
         <div class="col">
         	<a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/"><img alt="Licence Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc/4.0/88x31.png" /></a>
         </div>
-        <div class="col-8 text-center">
+        <div class="col-7 text-center">
 	        Ce site est mise à disposition selon les termes de la <br/><a rel="license" href="http://creativecommons.org/licenses/by-nc/4.0/">Licence Creative Commons Attribution - Pas d’Utilisation Commerciale 4.0 International</a>.
         </div >
         <div class="col-2">
