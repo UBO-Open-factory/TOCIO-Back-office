30807b4d11dcece29d7c7b14f85c7e3b845caa21
Alex <peretjatko@univ-brest.fr>
2020-05-15 17:30:31 +0200

    Ajustement de l'URL sur le 'home'

--- backoffice/basic/views/layouts/main.php
+++ backoffice/basic/views/layouts/main.php
@@ -98,7 +98,7 @@ AppAsset::register($this);
     
     // CONSTRUCTION DU MENU ------------------------------------------------------------------------
     NavBar::begin(['brandLabel' => Yii::$app->name,
-				    'brandUrl' => Url::to("/"),
+				    'brandUrl' => Url::base().Url::to("/"),
 				    'options' => [
 				    	'class' => 'navbar navbar-expand-lg ',
     				],
