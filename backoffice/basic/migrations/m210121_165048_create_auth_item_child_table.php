5a2f351b28897a9b558b5fd07f95cd8a1773221d
Alex <alex@alex-design.fr>
2021-02-09 17:55:19 +0100

    Script de migration pour la table auth_item_child2

--- backoffice/basic/migrations/m210121_165048_create_auth_item_child_table.php
+++ backoffice/basic/migrations/m210121_165048_create_auth_item_child_table.php
@@ -10,27 +10,26 @@ class m210121_165048_create_auth_item_child_table extends Migration {
 	 * {@inheritdoc}
 	 */
 	public function safeUp() {
-		$this->createTable( '{{%auth_item_child2}}', [
-				'parent' => $this->string( 64 )->notNull(),
-				'parent' => $this->primaryKey(),
-				'child' => $this->string( 64 )->notNull()->unique() ] );
+		$tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
 		
-		$this->createIndex( 'idx_parent', '{{%auth_item_child2}}', ['parent', 'child'] );
+// 		$this->createTable( '{{%auth_item_child2}}', [
+// 				'parent' => $this->string( 64 )->notNull(),
+// 				'parent' => $this->primaryKey(),
+// 				'child' => $this->string( 64 )->notNull()->unique() ] );
+		
+// 		$this->createIndex( 'idx_parent', '{{%auth_item_child2}}', ['parent', 'child'] );
 		
 		
 		// ****************************************************
 		// Voir le scipt de création dans vendor/yiisoft/yii2/rbac/migrations
 		// ****************************************************
 		
-// 		$this->createTable('{{%auth_item_child2}}', [
-// 				'parent' => $this->string(64)->notNull(),
-// 				'child' => $this->string(64)->notNull(),
-// 				'PRIMARY KEY ([[parent]], [[child]])',
-// 				'FOREIGN KEY ([[parent]]) REFERENCES ' . $authManager->itemTable . ' ([[name]])' .
-// 				$this->buildFkClause('ON DELETE CASCADE', 'ON UPDATE CASCADE'),
-// 				'FOREIGN KEY ([[child]]) REFERENCES ' . $authManager->itemTable . ' ([[name]])' .
-// 				$this->buildFkClause('ON DELETE CASCADE', 'ON UPDATE CASCADE'),
-// 		], $tableOptions);
+		$this->createTable( '{{%auth_item_child2}}', [
+				'parent' => $this->string( 64 )->notNull(),
+				'child' => $this->string( 64 )->notNull(),
+				'PRIMARY KEY ([[parent]], [[child]])',
+				'FOREIGN KEY ([[parent]]) REFERENCES {{%auth_item2}} ([[name]])'.$this->buildFkClause( 'ON DELETE CASCADE', 'ON UPDATE CASCADE' ),
+				'FOREIGN KEY ([[child]]) REFERENCES {{%auth_item2}} ([[name]])'.$this->buildFkClause( 'ON DELETE CASCADE', 'ON UPDATE CASCADE' ) ], $tableOptions);
 	}
 
 	/**
