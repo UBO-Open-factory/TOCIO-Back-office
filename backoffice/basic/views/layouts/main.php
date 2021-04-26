26b68265b54438f15e2ba7a1d1d5fa3a0498e957
Alex <alex@alex-design.fr>
2021-04-26 12:36:25 +0200

    Modification du footer

--- backoffice/basic/views/layouts/main.php
+++ backoffice/basic/views/layouts/main.php
@@ -137,13 +137,13 @@ AppAsset::register($this);
         </div >
         <?php }?>
         <div class="col">
-        	<a href="https://git.alex-design.fr/root/fablab.tocio.dataapi" target="_blank">
+        	<a href="https://github.com/UBO-Open-factory/TOCIO-Back-office" target="_blank">
         		<svg height="24" class="octicon octicon-mark-github" viewBox="0 0 16 16" version="1.1" width="24" aria-hidden="true"><path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0016 8c0-4.42-3.58-8-8-8z"></path></svg>
         		Dépot
         	</a>
         </div>
         <?php if( !Yii::$app->user->isGuest ){ ?>
-        <div class="col-2"><a href="https://bugs.alex-design.fr/" target="bug"><img src="<?php echo Url::to(['/assets/ico_BugTrackerAlexDesign.png']); ?>" height="30"></a></div>
+        <div class="col-2"><a href="https://github.com/UBO-Open-factory/TOCIO-Back-office/issues" target="bug">Ouvrir un bug</a></div>
         <?php }?>
         <div class="col pull-right"><?php echo Yii::powered()." ".Yii::getVersion() ?> </div>
     </div>
