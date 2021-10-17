86cdd74337b0be191918af6387aadd8177016c0c
Alex <alex@alex-design.fr>
2021-10-17 17:16:03 +0200

    Add the params config file

---
+++ backoffice/basic/config/params.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+		'adminEmail' => 'peretjatko@univ-brest.fr',
+		'senderEmail' => 'noreply@tocio.com',
+		'senderName' => 'Admin TOCIO',
+		'SMTP_host' 	=> "mailhost.univ-brest.fr",
+		'SMTP_port' 	=> "25",
+		'SMTP_username'	=> "",
+		'SMTP_password' => "",
+];
